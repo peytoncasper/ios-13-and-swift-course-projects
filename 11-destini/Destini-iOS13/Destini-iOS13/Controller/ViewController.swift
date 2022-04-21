020d7f0e1fb26e0036bbb89eba7ba7478e162ccc
Peyton Casper <peytoncas@gmail.com>
2022-04-20 22:29:10 -0400

    added destini app

---
+++ 11-destini/Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain: StoryBrain = StoryBrain()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        storyLabel.text = storyBrain.getStory().title
+
+        choice1Button.setTitle(storyBrain.getStory().choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.getStory().choice2, for: .normal)
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(choice: sender.currentTitle!)
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStory().title
+
+        choice1Button.setTitle(storyBrain.getStory().choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.getStory().choice2, for: .normal)
+    }
+    
+}
+
